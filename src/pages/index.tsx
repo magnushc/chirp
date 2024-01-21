96b491b53edef86c9d0855d1655e88e0dd552983
Magnus Haugom Christensen <magnus.haugom@gmail.com>
2024-01-21 23:27:41 +0100

    Next Image tag, styple up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
-import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { SignInButton, useUser } from "@clerk/nextjs";
 
-import { RouterOutputs, api } from "~/utils/api";
+import { type RouterOutputs, api } from "~/utils/api";
+
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+
+import Image from "next/image";
+
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -12,10 +19,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.imageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -31,14 +40,17 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img
+      <Image
         src={author.profilePictureUrl}
-        alt="Profile image"
+        alt="${author.username}'s profile picture"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <div className="flex flex-col">
-        <div className="flex text-slate-300">
+        <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
+          <span>{`· 1 ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
         <span>{post.content}</span>
       </div>
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,5 +1,5 @@
 import { auth, clerkClient } from "@clerk/nextjs";
-import { User } from "@clerk/nextjs/api";
+import { type User } from "@clerk/nextjs/api";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
@@ -49,7 +49,7 @@ export const postRouter = createTRPCRouter({
 
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
-      if (!author || !author.username)
+      if (!author?.username)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Author for post not found",
